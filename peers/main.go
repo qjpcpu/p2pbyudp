e9a540ae3a94a856d5d64771eaed8a655d4aed11
JasonQu <qjpcpu@gmail.com>
2018-01-26 18:12:00 +0800

    go

--- peers/main.go
+++ peers/main.go
@@ -32,31 +32,21 @@ func main() {
 		fmt.Printf("error during read: %s", err)
 	}
 	conn.Close()
-	anotherPeer, direction := parseAddr(string(data[:n]))
+	anotherPeer := parseAddr(string(data[:n]))
 	fmt.Printf("local:%s server:%s another:%s\n", srcAddr, remoteAddr, anotherPeer.String())
 
 	// 开始打洞
-	switch direction {
-	case "<->":
-		// 双向打洞
-		bidirectionHole(srcAddr, &anotherPeer)
-	case "->":
-		// 连接到另一节点
-		toPeerHole(srcAddr, &anotherPeer)
-	case "<-":
-		// 允许其他节点连接到我
-		fromPeerHole(srcAddr, &anotherPeer)
-	}
+	bidirectionHole(srcAddr, &anotherPeer)
 
 }
 
-func parseAddr(addr string) (net.UDPAddr, string) {
+func parseAddr(addr string) net.UDPAddr {
 	t := strings.Split(addr, ":")
-	port, _ := strconv.Atoi(t[2])
+	port, _ := strconv.Atoi(t[1])
 	return net.UDPAddr{
-		IP:   net.ParseIP(t[1]),
+		IP:   net.ParseIP(t[0]),
 		Port: port,
-	}, t[0]
+	}
 }
 
 func bidirectionHole(srcAddr *net.UDPAddr, anotherAddr *net.UDPAddr) {
@@ -66,36 +56,9 @@ func bidirectionHole(srcAddr *net.UDPAddr, anotherAddr *net.UDPAddr) {
 	}
 	defer conn.Close()
 	// 向另一个peer发送一条udp消息(对方peer的nat设备会丢弃该消息,非法来源),用意是在自身的nat设备打开一条可进入的通道,这样对方peer就可以发过来udp消息
-	if _, err = conn.Write([]byte("handshake from:" + tag)); err != nil {
+	if _, err = conn.Write([]byte(HAND_SHAKE_MSG)); err != nil {
 		log.Println("send handshake:", err)
-	} else {
-		log.Println("send handshake ok")
 	}
-
-	go func() {
-		for {
-			time.Sleep(10 * time.Second)
-			if _, err = conn.Write([]byte("from [" + tag + "]")); err != nil {
-				log.Println("send msg fail", err)
-			}
-		}
-	}()
-	for {
-		data := make([]byte, 1024)
-		n, remoteAddr, err := conn.ReadFromUDP(data)
-		if err != nil {
-			log.Printf("error during read: %s\n", err)
-		}
-		log.Printf("<%s> %s\n", remoteAddr, data[:n])
-	}
-}
-
-func toPeerHole(srcAddr *net.UDPAddr, anotherAddr *net.UDPAddr) {
-	conn, err := net.DialUDP("udp", srcAddr, anotherAddr)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer conn.Close()
 	for {
 		data := make([]byte, 1024)
 		n, _, err := conn.ReadFromUDP(data)
@@ -122,31 +85,6 @@ func toPeerHole(srcAddr *net.UDPAddr, anotherAddr *net.UDPAddr) {
 		if err != nil {
 			log.Printf("error during read: %s\n", err)
 		}
-		log.Printf("Read data:%s\n", data[:n])
-	}
-}
-
-func fromPeerHole(srcAddr *net.UDPAddr, anotherAddr *net.UDPAddr) {
-	conn, err := net.DialUDP("udp", srcAddr, anotherAddr)
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer conn.Close()
-	for {
-		if _, err = conn.Write([]byte(HAND_SHAKE_MSG)); err != nil {
-			log.Printf("发送握手消息失败:%v", err)
-		} else {
-			log.Println("发送握手消息成功")
-			break
-		}
-	}
-	for {
-		data := make([]byte, 1024)
-		n, _, err := conn.ReadFromUDP(data)
-		if err != nil {
-			log.Printf("error during read: %s\n", err)
-		}
-		log.Printf("Read data:%s\n", data[:n])
-		conn.Write([]byte("from " + tag))
+		log.Printf("收到数据:%s\n", data[:n])
 	}
 }
--- server/main.go
+++ server/main.go
@@ -4,19 +4,10 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"os"
 	"time"
 )
 
 func main() {
-	direction := os.Args[1]
-	// 双向打洞
-	if os.Args[1] == "bidirection" {
-		direction = "<->"
-	} else {
-		//单向打洞
-		direction = "->"
-	}
 	listener, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.IPv4zero, Port: 9981})
 	if err != nil {
 		fmt.Println(err)
@@ -33,15 +24,10 @@ func main() {
 		log.Printf("<%s> %s\n", remoteAddr.String(), data[:n])
 		peers = append(peers, *remoteAddr)
 		if len(peers) == 2 {
-			if direction == "->" {
-				log.Printf("进行单向打洞,建立 %s --> %s 的连接\n", peers[0].String(), peers[1].String())
-				listener.WriteToUDP([]byte("->:"+peers[1].String()), &peers[0])
-				listener.WriteToUDP([]byte("<-:"+peers[0].String()), &peers[1])
-			} else {
-				log.Printf("进行双向向打洞,建立 %s <--> %s 的连接\n", peers[0].String(), peers[1].String())
-				listener.WriteToUDP([]byte("<->:"+peers[1].String()), &peers[0])
-				listener.WriteToUDP([]byte("<->:"+peers[0].String()), &peers[1])
-			}
+
+			log.Printf("进行双向向打洞,建立 %s <--> %s 的连接\n", peers[0].String(), peers[1].String())
+			listener.WriteToUDP([]byte(peers[1].String()), &peers[0])
+			listener.WriteToUDP([]byte(peers[0].String()), &peers[1])
 			time.Sleep(time.Second * 8)
 			log.Println("中转服务器退出,仍不影响peers间通信")
 			return
