e80eff2b7d4956005951fe66357dd4b94e906b27
JasonQu <qjpcpu@gmail.com>
2018-01-26 18:02:06 +0800

    i

--- server/main.go
+++ server/main.go
@@ -4,16 +4,25 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
 	"time"
 )
 
 func main() {
+	direction := os.Args[1]
+	// 双向打洞
+	if os.Args[1] == "bidirection" {
+		direction = "<->"
+	} else {
+		//单向打洞
+		direction = "->"
+	}
 	listener, err := net.ListenUDP("udp", &net.UDPAddr{IP: net.IPv4zero, Port: 9981})
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	log.Printf("Local: <%s> \n", listener.LocalAddr().String())
+	log.Printf("本地地址: <%s> \n", listener.LocalAddr().String())
 	peers := make([]net.UDPAddr, 0, 2)
 	data := make([]byte, 1024)
 	for {
@@ -24,11 +33,17 @@ func main() {
 		log.Printf("<%s> %s\n", remoteAddr.String(), data[:n])
 		peers = append(peers, *remoteAddr)
 		if len(peers) == 2 {
-			log.Printf("got 2 peers, start p2p test:%+v", peers)
-			listener.WriteToUDP([]byte(peers[0].String()), &peers[1])
-			listener.WriteToUDP([]byte(peers[1].String()), &peers[0])
+			if direction == "->" {
+				log.Printf("进行单向打洞,建立 %s --> %s 的连接\n", peers[0].String(), peers[1].String())
+				listener.WriteToUDP([]byte("->:"+peers[1].String()), &peers[0])
+				listener.WriteToUDP([]byte("<-:"+peers[0].String()), &peers[1])
+			} else {
+				log.Printf("进行双向向打洞,建立 %s <--> %s 的连接\n", peers[0].String(), peers[1].String())
+				listener.WriteToUDP([]byte("<->:"+peers[1].String()), &peers[0])
+				listener.WriteToUDP([]byte("<->:"+peers[0].String()), &peers[1])
+			}
 			time.Sleep(time.Second * 8)
-			log.Println("server exit.")
+			log.Println("中转服务器退出,仍不影响peers间通信")
 			return
 		}
 	}
